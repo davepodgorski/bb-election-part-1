46bd9a999718169b5d26a6e56d8ba26308dd0826
davepodgorski <davepodgorski@gmail.com>
2019-01-07 19:19:29 -0500

    election

--- election.js
+++ election.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const electionResults = document.querySelector('#electionResults');
 
-  // Imagination!
+  const resultsRequest = axios({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    responseType: 'text'
+  }).then(function (response) {
+    response.data.candidates.forEach(function(candidate) {
+    const candidateResult = document.createElement('LI')
+    candidateResult.innerHTML = (`${candidate.name}: ${candidate.votes} votes`)
+    electionResults.appendChild(candidateResult);
+    })
 
+    console.log(response.data);
+
+  })
 });
